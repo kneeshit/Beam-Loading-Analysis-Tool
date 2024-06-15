433b2090a5dad85a8e663bb7cad6831bbb88eeda
Nishit Mistry <117573373+kneeshit@users.noreply.github.com>
2024-06-15 22:46:48 +0530

    Add files via upload

--- moscode.py
+++ moscode.py
@@ -43,6 +43,8 @@ def values(f1,f2,f3,f4,a,b,length,G,I):
       p += m(f, a, i[1], i[2], n=10000)
       q += integral(f, i[1], i[2], n=10000)
 
+  f2c=f2.copy()
+
   r2 = p/(a-b)
   r1 = -q - r2
 
@@ -51,6 +53,7 @@ def values(f1,f2,f3,f4,a,b,length,G,I):
   f2.sort(key = lambda x: x[1])
   f3.sort(key = lambda x: x[1])
   f4.sort(key = lambda x: x[1])
+
   l = np.linspace(0, length, 1001)
   dl = l[1]-l[0]
   i1 = i2 = i3 = i4 = -1
@@ -148,14 +151,16 @@ def values(f1,f2,f3,f4,a,b,length,G,I):
       ai = i
     if l[i] == b:
       bi = i
+  for i in range(len(l)):
+    deflection[i] /= (G*I)
+    slope[i] /= (G*I)
+
   c2 = (a*deflection[bi]-b*deflection[ai])/(b-a)
   c1 = (deflection[ai]-deflection[bi])/(b-a)
   for i in range(len(l)):
     deflection[i] += c1*l[i]+c2
-    deflection[i] /= (G*I)
     slope[i] += c1
-    slope[i] /= (G*I)
-  return V,BM,slope,deflection
+  return V,BM,slope,deflection,f2c
 
 def draw_beam(l, support1, support2, f1, f2, f3, f4):
     image = Image.new("RGB", (400, 400), "pink")
@@ -178,7 +183,7 @@ def draw_beam(l, support1, support2, f1, f2, f3, f4):
         bounding_box = (moment_location-25, 175, moment_location+25, 225)
         start_angle = -130
         end_angle = 130
-        value = str(abs(i[0]))
+        value = str(round(abs(i[0]),2))
         draw.arc(bounding_box, start=start_angle, end=end_angle, fill="black", width=3)
         draw.text([moment_location, 150], value, fill="black", font_size=20)
         if i[0]<0:
@@ -191,7 +196,7 @@ def draw_beam(l, support1, support2, f1, f2, f3, f4):
     # Draw the point forces
     for i in f2:
         load_location = i[1]*300/l+50
-        value = str(abs(i[0]))
+        value = str(round(abs(i[0]),2))
         draw.text([load_location, 130], value, fill="black", font_size=20)
         if i[0]<0:
             draw.line([load_location, 155, load_location, 190], fill='black', width=3)
@@ -206,7 +211,7 @@ def draw_beam(l, support1, support2, f1, f2, f3, f4):
     for i in f3:
         load_start_location = i[1]*300/l+50
         load_end_location = i[2]*300/l+50
-        value = str(abs(i[0]))
+        value = str(round(abs(i[0]), 2))
         draw.rectangle([(load_start_location, 155), (load_end_location, 190)], outline="black", fill=(163, 163, 163, 40), width=2)
         draw.text([(load_end_location+load_start_location)/2, 130], value, fill="black", font_size=20)
         if i[0]<0:
@@ -220,33 +225,12 @@ def draw_beam(l, support1, support2, f1, f2, f3, f4):
             draw.line([load_end_location+0.5, 155, load_end_location+15*np.cos(np.pi/4), 155+15*np.sin(np.pi/4)], fill='black', width=3)
             draw.line([load_end_location+0.5, 155, load_end_location-15*np.cos(np.pi/4), 155+15*np.sin(np.pi/4)], fill='black', width=3)
 
-        # if load_end_location - load_start_location < 30:
-        #     draw.line([load_start_location, 155, load_start_location, 190], fill='black', width=3)
-        #     draw.line([load_start_location+0.5, 190, load_start_location+15*np.cos(np.pi/4), 190-15*np.sin(np.pi/4)], fill='black', width=3)
-        #     draw.line([load_start_location+0.5, 190, load_start_location-15*np.cos(np.pi/4), 190-15*np.sin(np.pi/4)], fill='black', width=3)
-        #     draw.line([load_end_location, 155, load_end_location, 190], fill='black', width=3)
-        #     draw.line([load_end_location+0.5, 190, load_end_location+15*np.cos(np.pi/4), 190-15*np.sin(np.pi/4)], fill='black', width=3)
-        #     draw.line([load_end_location+0.5, 190, load_end_location-15*np.cos(np.pi/4), 190-15*np.sin(np.pi/4)], fill='black', width=3)
-        # else:
-        #     draw.line([load_start_location, 155, load_start_location, 190], fill='black', width=3)
-        #     draw.line([load_start_location+0.5, 190, load_start_location+15*np.cos(np.pi/4), 190-15*np.sin(np.pi/4)], fill='black', width=3)
-        #     draw.line([load_start_location+0.5, 190, load_start_location-15*np.cos(np.pi/4), 190-15*np.sin(np.pi/4)], fill='black', width=3)
-        #     draw.line([load_end_location, 155, load_end_location, 190], fill='black', width=3)
-        #     draw.line([load_end_location+0.5, 190, load_end_location+15*np.cos(np.pi/4), 190-15*np.sin(np.pi/4)], fill='black', width=3)
-        #     draw.line([load_end_location+0.5, 190, load_end_location-15*np.cos(np.pi/4), 190-15*np.sin(np.pi/4)], fill='black', width=3)
-        #     n = abs(load_end_location - load_start_location)//30
-        #     for j in range(n):
-        #         draw.line([load_start_location+30*n, 155, load_start_location+30*n, 190], fill='black', width=3)
-        #         draw.line([load_start_location+30*n+0.5, 190, load_start_location+30*n+15*np.cos(np.pi/4), 190-15*np.sin(np.pi/4)], fill='black', width=3)
-        #         draw.line([load_start_location+30*n+0.5, 190, load_start_location+30*n-15*np.cos(np.pi/4), 190-15*np.sin(np.pi/4)], fill='black', width=3)
-
-
     # Draw the triangular force profile
     for i in f4:
         load_start_location = i[1]*300/l+50
         load_end_location = i[2]*300/l+50
         slope = 55/(load_end_location-load_start_location)
-        value = str(abs(i[0]))
+        value = str(round(abs(i[0]), 2))
         draw.polygon([load_start_location, 190, load_end_location, 190-slope*(load_end_location-load_start_location), load_end_location, 190], outline="black", fill=(163, 163, 163, 40), width=2)
         draw.text([load_end_location, 115], value, fill="black", font_size=20)
         if i[0]<0:
@@ -255,25 +239,7 @@ def draw_beam(l, support1, support2, f1, f2, f3, f4):
         else:
             draw.line([load_end_location+0.5, 190, load_end_location+15*np.cos(np.pi/4), 155+15*np.sin(np.pi/4)], fill='black', width=3)
             draw.line([load_end_location+0.5, 190, load_end_location-15*np.cos(np.pi/4), 155+15*np.sin(np.pi/4)], fill='black', width=3)
-        # if load_end_location - load_start_location < 30:
-        #     # draw.line([load_start_location, 155, load_start_location, 190], fill='black', width=3)
-        #     # draw.line([load_start_location+0.5, 190, load_start_location+15*np.cos(np.pi/4), 190-15*np.sin(np.pi/4)], fill='black', width=3)
-        #     # draw.line([load_start_location+0.5, 190, load_start_location-15*np.cos(np.pi/4), 190-15*np.sin(np.pi/4)], fill='black', width=3)
-        #     draw.line([load_end_location, 155-slope*(load_end_location-load_start_location), load_end_location, 190], fill='black', width=3)
-        #     draw.line([load_end_location+0.5, 190, load_end_location+15*np.cos(np.pi/4), 190-15*np.sin(np.pi/4)], fill='black', width=3)
-        #     draw.line([load_end_location+0.5, 190, load_end_location-15*np.cos(np.pi/4), 190-15*np.sin(np.pi/4)], fill='black', width=3)
-        # else:
-        #     # draw.line([load_start_location, 155, load_start_location, 190], fill='black', width=3)
-        #     # draw.line([load_start_location+0.5, 190, load_start_location+15*np.cos(np.pi/4), 190-15*np.sin(np.pi/4)], fill='black', width=3)
-        #     # draw.line([load_start_location+0.5, 190, load_start_location-15*np.cos(np.pi/4), 190-15*np.sin(np.pi/4)], fill='black', width=3)
-        #     draw.line([load_end_location, 155-slope*(load_end_location-load_start_location), load_end_location, 190], fill='black', width=3)
-        #     draw.line([load_end_location+0.5, 190, load_end_location+15*np.cos(np.pi/4), 190-15*np.sin(np.pi/4)], fill='black', width=3)
-        #     draw.line([load_end_location+0.5, 190, load_end_location-15*np.cos(np.pi/4), 190-15*np.sin(np.pi/4)], fill='black', width=3)
-        #     n = abs(load_end_location - load_start_location)//30
-        #     for j in range(n):
-        #         draw.line([load_start_location+30*n, 155-slope*30*n, load_start_location+30*n, 190], fill='black', width=3)
-        #         draw.line([load_start_location+30*n+0.5, 190, load_start_location+30*n+15*np.cos(np.pi/4), 190-15*np.sin(np.pi/4)], fill='black', width=3)
-        #         draw.line([load_start_location+30*n+0.5, 190, load_start_location+30*n-15*np.cos(np.pi/4), 190-15*np.sin(np.pi/4)], fill='black', width=3)
+
     return image
 
 def main():
@@ -295,46 +261,54 @@ def main():
     if 'deflection' not in st.session_state:
         st.session_state.deflection = []
     if 'G' not in st.session_state:
-        st.session_state.G = 0
+        st.session_state.G = 1
     if 'I' not in st.session_state:
-        st.session_state.I = 0
+        st.session_state.I = 1
 
     ## layout
     col = st.columns((2, 1), gap='medium')
     with col[0]:
+        ## G value
+        G = st.number_input("Modulus of Elasticity 'E' (Pa):",step=0.00001, key="G_value")
+
+        ## I value
+        I = st.number_input("Second Moment of Area 'I' (m⁴):",step=0.01, key="I_value")
+
+        st.divider()
 
         ## beam length
-        st.subheader("Enter beam length:")
-        beam_length = st.slider("meters:", min_value=0, max_value=100, value=50)
-        "Beam Length:", beam_length,"m" 
+        beam_length = st.slider("Enter Beam Length (m):",min_value=0, max_value=100, value=50)
+        st.write("Beam Length:", beam_length,"m")
+
+        st.divider()
 
         ## support location
-        st.subheader("Enter supports location:")
-        support_locations = st.slider("meters:", min_value=0, max_value=beam_length, value=[beam_length//4,3*beam_length//4])
+        support_locations = st.slider("Enter Support Locations (m):", min_value=0, max_value=beam_length, value=[beam_length//4,3*beam_length//4])
         support_1 = support_locations[0]
         support_2 = support_locations[1]
-        st.write("Support locations:", support_1, ",", support_2)
+        st.write("Support Locations:",support_1, "m and", support_2,"m")
+
+        st.divider()
 
         ## load type
-        st.subheader("Select load type:")
         load_types = ["Point Moment", "Point Force", "Constant Force Profile", "Triangular Force Profile"]
-        load_type = option_menu("Load type:", load_types, orientation="horizontal") 
+        load_type = option_menu("Select load type:", load_types, orientation="horizontal") 
 
         if load_type == "Point Moment":
-            moment_value = st.number_input("Enter moment value",step=1, key="point_moment_value")
-            load_location = st.slider("meters:", min_value=0, max_value=beam_length, value=beam_length//2, key="point_moment_location")
+            moment_value = st.number_input("Enter Point Moment Magnitude (N-m):",step=0.01, key="point_moment_value")
+            load_location = st.slider("Enter Point Moment Location (m):", min_value=0, max_value=beam_length, value=beam_length//2, key="point_moment_location")
 
         elif load_type == "Point Force":
-            force_value = st.number_input("Enter force value",step=1, key="point_force_value")
-            load_location = st.slider("meters:", min_value=0, max_value=beam_length, value=beam_length//2, key="point_force_location")
+            force_value = st.number_input("Enter Point Force Magnitude (N):",step=0.01, key="point_force_value")
+            load_location = st.slider("Enter Point Force Location (m):", min_value=0, max_value=beam_length, value=beam_length//2, key="point_force_location")
 
         elif load_type == "Constant Force Profile":
-            w_value = st.number_input("Enter w value",step=1, key="constant_force_profile_value")
-            load_range = st.slider("meters:", min_value=0, max_value=beam_length, value=[beam_length//4,3*beam_length//4], key="constant_force_profile_location")
+            w_value = st.number_input("Enter 'w' Magnitude (N/m):",step=0.01, key="constant_force_profile_value")
+            load_range = st.slider("Enter Constant Force Profile Range (m):", min_value=0, max_value=beam_length, value=[beam_length//4,3*beam_length//4], key="constant_force_profile_location")
 
         elif load_type == "Triangular Force Profile":
-            w0_value = st.number_input("Enter w0 value",step=1, key="triangular_force_profile_value")
-            load_range = st.slider("meters:", min_value=0, max_value=beam_length, value=[beam_length//4,3*beam_length//4], key="triangular_force_profile_location")
+            w0_value = st.number_input("Enter 'w₀' Magnitude (N):",step=0.01, key="triangular_force_profile_value")
+            load_range = st.slider("Enter Triangular Force Profile Range (m):", min_value=0, max_value=beam_length, value=[beam_length//4,3*beam_length//4], key="triangular_force_profile_location")
 
         add_load = st.button("Add load")
         if add_load:
@@ -353,34 +327,87 @@ def main():
             elif load_type == "Triangular Force Profile":
                 st.session_state.f4.append([w0_value,load_range[0],load_range[1]])
                 add_load = False
-                
-        done = st.button("Done with Force")
+        
+        st.divider()
+
+        done = st.button("Done")
+
+        st.divider()
+
         if done:
-            V,BM,slope,deflection=values(st.session_state.f1,st.session_state.f2,st.session_state.f3,st.session_state.f4,support_1,support_2,beam_length,1,1)
+            V,BM,slope,deflection,f2c=values(st.session_state.f1,st.session_state.f2,st.session_state.f3,st.session_state.f4,support_1,support_2,beam_length,1,1)
             col2 = st.columns((3,3), gap='medium')
+            x_axis = []
+            length_distribution = np.linspace(0,beam_length,1001)
 
             fig1,ax1=plt.subplots()
-            ax1.plot(V) # take value from backend
+            ax1.plot(length_distribution,V) # take value from backend
+            ax1.set_title("Shear Force Diagram")
+            ax1.set_xlabel("Beam")
+            ax1.set_ylabel("Magnitude (N/m²)")
+            ax1.axhline(0, c='r')
+            ax1.axvline(support_1, c='g', label="Support 1", linestyle='--')
+            ax1.axvline(support_2, c='k', label="Support 2",  linestyle='--')
+            ax1.grid()
+            ax1.legend()
             st.write("Shear Force Diagram")
             st.pyplot(fig1, width=400)
+            v_max = max(map(abs,V))
+            st.write("Maximum Shear Force Value:",v_max,"N/m²")
+
+            st.divider()
 
             fig2,ax2=plt.subplots()
-            ax2.plot(BM) # take value from backend
+            ax2.plot(length_distribution,BM) # take value from backend
+            ax2.set_title("Bending Moment Diagram")
+            ax2.set_xlabel("Beam")
+            ax2.set_ylabel("Magnitude (N-m)")
+            ax2.axhline(0, c='r')
+            ax2.axvline(support_1, c='g', label="Support 1", linestyle='--')
+            ax2.axvline(support_2, c='k', label="Support 2",  linestyle='--')
+            ax2.grid()
+            ax2.legend()
             st.write("Bending Moment Diagram")
             st.pyplot(fig2, width=400)
+            bm_max = max(map(abs,BM))
+            st.write("Maximum Bending Moment Value:",bm_max,"N-m")
 
+            st.divider()
+            
 
             fig4,ax4=plt.subplots()
-            ax4.plot(deflection) # take value from backend
+            ax4.plot(length_distribution,deflection) # take value from backend
+            ax4.set_title("Deflection(y)")
+            ax4.set_xlabel("Beam")
+            ax4.set_ylabel("Magnitude (m)")
+            ax4.axhline(0, c='r')
+            ax4.axvline(support_1, c='g', label="Support 1", linestyle='--')
+            ax4.axvline(support_2, c='k', label="Support 2",  linestyle='--')
+            ax4.grid()
+            ax4.legend()
             st.write("Deflection of Beam")
             st.pyplot(fig4, width=400)
+            deflection_max = max(map(abs,deflection))
+            st.write("Maximum Delfection:",deflection_max,"m")
+
+            st.divider()
+            
 
             fig3,ax3=plt.subplots()
-            ax3.plot(slope) # take value from backend
+            ax3.plot(length_distribution,slope) # take value from backend
+            ax3.set_title("Slope")
+            ax3.set_xlabel("Beam")
+            ax3.set_ylabel("Magnitude")
+            ax3.axhline(0, c='r')
+            ax3.axvline(support_1, c='g', label="Support 1", linestyle='--')
+            ax3.axvline(support_2, c='k', label="Support 2",  linestyle='--')
+            ax3.grid()
+            ax3.legend()
             st.write("Slope of beam")
             st.pyplot(fig3, width=400)
+            slope_max = max(map(abs,slope))
+            st.write("Maximum Slope:",slope_max)
 
-    
     with col[1]:
         ## real time schematic
         image = draw_beam(beam_length, support_1, support_2, st.session_state.f1, st.session_state.f2, st.session_state.f3, st.session_state.f4)
@@ -388,4 +415,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
